11c7b223230a2b4327735a71a1ffc0e341fdd8ca
lnikolina <lekaj0102@gmail.com>
2023-05-11 19:17:28 +0200

    postavljen discord klijent

--- security_monitoring.py
+++ security_monitoring.py
@@ -4,11 +4,12 @@ import RPi.GPIO as GPIO  # biblioteka za ulaz i izlaz GPIO pinova
 # import numpy as np
 # import cv2
 import discord
-# from picamera import PiCamera
+from picamera import PiCamera
 import picamera
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import Intents
 
 print("Hello, this is my project!")
 
@@ -20,6 +21,16 @@ GPIO.setmode(GPIO.BCM)
 PIR_PIN = 7
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-# postavljanje kamere
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+# postavljanje kamere - trenutna greška u kodu s obziom na faleći ribbon kabel za kameru
+# camera = picamera.PiCamera()
+# camera.resolution = (640, 480)
+
+
+# postavljanje discord klijenta
+intents = Intents.default()  # postavljanje zadanog skupa namjeri
+intents.members = True  # ova linija koda dopusta botu da prima clanove dogadjaja
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
